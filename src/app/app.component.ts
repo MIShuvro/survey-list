897fc1ca7c39a02e529f2237811c05d6086a9d8f
hasibul-99 <hasibul_uap@outlook.com>
2022-02-26 00:17:32 +0600

    servey lis show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,45 @@
 import { Component } from '@angular/core';
 
+type question = {
+  title: string,
+  type: string,
+  options: object,
+}
+export interface PeriodicElement {
+  id: number;
+  title: string;
+  data: string;
+  time: string;
+  questions: question[];
+}
+
+const SURVETS_LIST: PeriodicElement[] = [
+  {
+    id: 1,
+    title: "Title of survey",
+    data: "12/1/2022",
+    time: "30min",
+    questions: [
+      {
+        title: "Actual question",
+        type: "radio",
+        options: ['Male', 'Female']
+      },
+      {
+        title: "Actual question2",
+        type: "text",
+        options: []
+      },
+    ]
+  }
+]
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-app';
+  displayedColumns: string[] = [ 'Created Date', 'Survey Title', 'Number of Questions', 'Time', 'Action'];
+  dataSource = SURVETS_LIST;
 }
